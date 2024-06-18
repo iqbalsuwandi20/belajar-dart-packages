b9d5fdbf37e3d0f3a2e62268f127670b6af8b3df
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-18 16:57:51 +0700

    belajar dart packages

---
+++ example/hello.dart
@@ -0,0 +1,15 @@
+import 'package:belajar_dart_packages/hello.dart';
+
+void main() {
+
+  print(sayHello("Iqbal"));
+
+  Category category = Category();
+  Product product = Product();
+  Customer customer = Customer();
+
+  print(category);
+  print(product);
+  print(customer);
+  
+}
